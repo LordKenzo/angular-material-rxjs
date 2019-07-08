d51220ea8381ed7e84b0099e94616e3b322b90b9
LordKenzo <lordkenzo@outlook.com>
2019-07-08 14:13:34 +0200

    Now is working with Observables and RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { fromEvent, from } from 'rxjs';
+import { map, take, filter, debounceTime, distinct, distinctUntilChanged, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements AfterViewInit  {
   name = 'Angular';
   searchTerm: string;
   results;
   url = 'https://api.github.com/search/repositories?q=topic';
-
+  @ViewChild('searchRef', {static: false}) searchRef: ElementRef;
+ 
   constructor(private http: HttpClient) {}
 
-  // Con questo approccio posso avere problemi con le API di github perchè richiedo dati in maniera continua!!!
+  ngAfterViewInit() {
+    fromEvent(this.searchRef.nativeElement, 'keyup').pipe(
+      map((event: any) => {
+        return event.target.value;
+      }),
+      filter(res => res.length > 2),
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe((text: string) => 
+        this.results =this.newSearch(text)
+    );
+  }
 
   newSearch(search) {
-    this.results = this.http.get(`${this.url}:${search}&sort=stars&order=desc`).pipe(
-      map(res => res['items'].map(item => item.name))
+    return this.http.get(`${this.url}:${search}&sort=stars&order=desc`).pipe(
+       map(res => res['items'].map(item => item.name))
     );
   }
 }
