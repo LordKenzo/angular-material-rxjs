99111901d6822169fde6c6085b6c97f785657a6b
LordKenzo <lordkenzo@outlook.com>
2019-07-08 11:23:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-rxjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-rxjs)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule  } from '@angular/material/toolbar';
+import { MatInputModule} from '@angular/material/input';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
