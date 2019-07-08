b6faf02731649e06013e6ebf11a84f095c21ec61
LordKenzo <lordkenzo@outlook.com>
2019-07-08 12:00:37 +0200

    Request data from github API with some problems.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -8,8 +10,16 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
   searchTerm: string;
+  results;
+  url = 'https://api.github.com/search/repositories?q=topic';
+
+  constructor(private http: HttpClient) {}
+
+  // Con questo approccio posso avere problemi con le API di github perchè richiedo dati in maniera continua!!!
 
   newSearch(search) {
-    console.log(search);
+    this.results = this.http.get(`${this.url}:${search}&sort=stars&order=desc`).pipe(
+      map(res => res['items'].map(item => item.name))
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule  } from '@angular/material/toolbar';
 import { MatInputModule} from '@angular/material/input';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatInputModule,
   ],
